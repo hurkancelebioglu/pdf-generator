a797536e5d9cebb40e9a8b74262104aa30fc88a6
hurkan <hurkan.celebioglu@gmail.com>
2023-09-29 15:46:33 +0300

    Final

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
     pdf.add_page()
 
-    # Set the header
+
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
 
-    # Set the footer
+
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
